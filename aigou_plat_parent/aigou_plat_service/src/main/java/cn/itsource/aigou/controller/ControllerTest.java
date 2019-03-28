03d2485a43f969ac3160913e7b1d82921b85dc88
admin <admin@qq.com>
2019-03-28 14:06:58 +0800

    冲突测试

--- aigou_plat_parent/aigou_plat_service/src/main/java/cn/itsource/aigou/controller/ControllerTest.java
+++ aigou_plat_parent/aigou_plat_service/src/main/java/cn/itsource/aigou/controller/ControllerTest.java
@@ -2,6 +2,6 @@ package cn.itsource.aigou.controller;
 
 public class ControllerTest {
     public static void main(String[] args) {
-        System.out.println("哈哈哈");
+        System.out.println("哈哈哈哈");
     }
 }
